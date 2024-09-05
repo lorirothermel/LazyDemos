c73d74299d9c00d658f4ec54ec9f697544d29de9
lorirothermel <lori@rothermel1.com>
2024-09-05 15:28:12 -0400

    Initial Commit

---
+++ LazyDemos/LazyDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyDemosApp.swift
+//  LazyDemos
+//
+//  Created by Lori Rothermel on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
